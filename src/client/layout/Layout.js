816f283dc1e185861f81477b9750703328f7d2c4
crolim-vbi <crolim@vbirealestate.com>
2020-12-08 00:38:23 -0300

    refinamento

--- src/client/layout/Layout.js
+++ src/client/layout/Layout.js
@@ -5,8 +5,6 @@ import Footer from '../partials/Footer';
 
 
 
-
-
 const Titulo = (props) => {
   return(
     <h2 style={{fontWeight: "550", marginTop: "0px"}}>{props.titulo}</h2>
@@ -23,7 +21,7 @@ const Layout = (props) => {
         <div className="mainWrap">
           <div className="maxScreenWidth">
             
-            <br></br>
+            <div style={{height: "25px"}}></div>
       
             <Titulo titulo={props.titulo}/>
             
