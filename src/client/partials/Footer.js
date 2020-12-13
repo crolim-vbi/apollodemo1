895f86c9b2566f08cdb6049e710f9b8d20954c7c
crolim-vbi <crolim@vbirealestate.com>
2020-12-12 21:49:35 -0300

    refinamento

--- src/client/partials/Footer.js
+++ src/client/partials/Footer.js
@@ -47,25 +47,22 @@ const Footer = function() {
   const bull = <span className={classes2.bullet}>•</span>;
   
     return (
-    
-    <>
   
-        <div className="bottomWrap">
-          <div className="footer1 maxScreenWidth">
-            <img src={logo_vbi} height="35px" />
-            <div style={{height: '100%', textAlign: 'right'}}>
-              <div style={{padding: '12px 0px 8px'}}> apollo@vbirealestate.com </div>
-              <div> 55 11 2344 - 2525 </div>
-            </div>
+      <div className="bottomWrap">
+        <div className="footer1 maxScreenWidth">
+          <img src={logo_vbi} height="35px" />
+          <div style={{height: '100%', textAlign: 'right'}}>
+            <div style={{padding: '12px 0px 8px'}}> apollo@vbirealestate.com </div>
+            <div> 55 11 2344 - 2525 </div>
           </div>
-          <div className="footer2 maxScreenWidth">
-            <div style={{padding: '12px 0px 8px'}}> COPYRIGHT © 2017 - 2020 - VBI REAL ESTATE</div>
-            <div> Apollo27 é um sistema desenvolvido pela VBI Real Estate </div>
-          </div>       
         </div>
-      
-    </>
-    
+        <div className="footer2 maxScreenWidth">
+          <div style={{padding: '12px 0px 8px'}}> COPYRIGHT © 2017 - 2020 - VBI REAL ESTATE</div>
+          <div> Apollo27 é um sistema desenvolvido pela VBI Real Estate </div>
+        </div>       
+      </div>
+  
+  
     );
   }
 
