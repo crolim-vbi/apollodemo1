4f7ace853d88776c0832d965370254e2b86e75b3
crolim-vbi <crolim@vbirealestate.com>
2020-12-20 11:29:10 -0300

    Add paleta com cores VBI: instucionais e de apoio

---
+++ src/client/CoresVbi.js
@@ -0,0 +1,110 @@
+const CoresVBI = {
+
+    // Cores Institucionais
+
+    pretoVbi :{
+        light: '#2c2c2c',
+        main: '#000000',
+        dark: '#000000',
+        contrastText: '#fff',
+    },
+    cinzaVbi1 : {
+        light: '#909292',
+        main: '#636464',
+        dark: '#393a3a',
+        contrastText: '#fff',
+    },
+
+    cinzaVbi2 :{
+        light: '#a6a7aa',
+        main: '#77787b',
+        dark: '#4b4c4f',
+        contrastText: '#fff',
+    },
+
+
+    cinzaVbi3 :{
+        light: '#cacccf',
+        main: '#999b9e',
+        dark: '#6b6d70',
+        contrastText: '#fff',
+    },
+
+
+    cinzaVbi4 :{
+        light: '#eeeff3',
+        main: '#bcbdc0',
+        dark: '#8c8d90',
+        contrastText: '#fff',
+    },
+
+
+
+    cinzaVbi5 :{
+        light: '#ffffff',
+        main: '#d1d3d4',
+        dark: '#a0a2a3',
+        contrastText: '#fff',
+    },
+
+
+    azulFakeVbiDark :{
+        light: '#4a83bd',
+        main: '#00578d',
+        dark: '#002f5f',
+        contrastText: '#fff',
+    },
+
+
+    // Cores De Apoio
+
+
+    laranjaVbi1 :{
+        light: '#ffce53',
+        main: '#f99d1c',
+        dark: '#c06e00',
+        contrastText: '#fff',
+    },
+
+
+    laranjaVbi2 :{
+        light: '#fff453',
+        main: '#ffc20e',
+        dark: '#c79200',
+        contrastText: '#fff',
+    },
+
+
+    azulVbi1 :{
+        light: '#66e5ff',
+        main: '#00b3f0',
+        dark: '#0083bd',
+        contrastText: '#fff',
+    },
+
+
+    azulVbi2 :{
+        light: '#a2acf0',
+        main: '#717dbd',
+        dark: '#41518d',
+        contrastText: '#fff',
+    },
+
+    verdePetroleo :{
+        light: '#58dbce',
+        main: '#00a99d',
+        dark: '#00796f',
+        contrastText: '#fff',
+    },
+
+    verdeFogo :{
+        light: '#e8ff6a',
+        main: '#b3d335',
+        dark: '#80a200',
+        contrastText: '#000'
+    },
+
+
+}
+
+export default CoresVBI
--- src/client/index.js
+++ src/client/index.js
@@ -1,129 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
+import './App.css';
 
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import { orange, green } from '@material-ui/core/colors';
-
-import Checkbox from '@material-ui/core/Checkbox';
-
-// Cores Institucionais
-
-const pretoVbi ={
-  light: '#2c2c2c',
-  main: '#000000',
-  dark: '#000000',
-  contrastText: '#fff',
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 
 
-const cinzaVbi1 ={
-  light: '#909292',
-  main: '#636464',
-  dark: '#393a3a',
-  contrastText: '#fff',
-}
-
-const cinzaVbi2 ={
-  light: '#a6a7aa',
-  main: '#77787b',
-  dark: '#4b4c4f',
-  contrastText: '#fff',
-}
-
-
-const cinzaVbi3 ={
-  light: '#cacccf',
-  main: '#999b9e',
-  dark: '#6b6d70',
-  contrastText: '#fff',
-}
-
-
-const cinzaVbi4 ={
-  light: '#eeeff3',
-  main: '#bcbdc0',
-  dark: '#8c8d90',
-  contrastText: '#fff',
-}
-
-
-
-const cinzaVbi5 = {
-  light: '#ffffff',
-  main: '#d1d3d4',
-  dark: '#a0a2a3',
-  contrastText: '#fff',
-}
-
-
-const azulFakeVbiDark = {
-  light: '#4a83bd',
-  main: '#00578d',
-  dark: '#002f5f',
-  contrastText: '#fff',
-}
-
-
-// Cores De Apoio
-
-
-const laranjaVbi1 = {
-  light: '#ffce53',
-  main: '#f99d1c',
-  dark: '#c06e00',
-  contrastText: '#fff',
-}
-
-
-const laranjaVbi2 = {
-  light: '#fff453',
-  main: '#ffc20e',
-  dark: '#c79200',
-  contrastText: '#fff',
-}
-
-
-const azulVbi1 = {
-  light: '#66e5ff',
-  main: '#00b3f0',
-  dark: '#0083bd',
-  contrastText: '#fff',
-}
-
-
-const azulVbi2 = {
-  light: '#a2acf0',
-  main: '#717dbd',
-  dark: '#41518d',
-  contrastText: '#fff',
-}
-
-const verdePetroleo ={
-  light: '#58dbce',
-  main: '#00a99d',
-  dark: '#00796f',
-  contrastText: '#fff',
-}
-
-const verdeFogo = {
-  light: '#e8ff6a',
-  main: '#b3d335',
-  dark: '#80a200',
-  contrastText: '#000'
-}
-
+import CoresVbi from './CoresVbi';
+import { createMuiTheme,  ThemeProvider } from '@material-ui/core/styles';
 
 
 const theme = createMuiTheme({
   palette: {
-    primary: cinzaVbi1,
-    secondary: verdePetroleo,
+    primary: CoresVbi.cinzaVbi1,
+    secondary: CoresVbi.verdePetroleo,
   },
 });
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
@@ -131,7 +24,6 @@ ReactDOM.render(
     <Router>
       <Switch>
         <HashRouter>
-          {/* <Checkbox /> */}
           <Route path="/" component={App} />
         </HashRouter>
       </Switch>
@@ -139,9 +31,6 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// render(<Sample />, document.getElementById('root'));
-
 
 
 
