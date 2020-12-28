2b11f70b3e993a5977555403fce64bb6a99e6d81
crolim-vbi <crolim@vbirealestate.com>
2020-12-27 22:49:34 -0300

    refinamento

--- src/client/partials/Classificacao.js
+++ src/client/partials/Classificacao.js
@@ -93,9 +93,9 @@ export default function BasicTable() {
 
       <Grid container spacing={0} style={{color:"dimgray"}}>
         
-        <Grid item xs={12} md={12}>
+        {/* <Grid item xs={12} md={12}>
           <div style={{marginBottom: "10px"}}></div>
-        </Grid>    
+        </Grid>     */}
 
         <Grid item xs={12} md={12} style={{borderTop: "solid 1px lightgray", borderBottom: "solid 1px lightgray2"}}>
           <div style={{width:"100%", height:"100%", display: "flex", justifyContent: "flex-start"}}>
