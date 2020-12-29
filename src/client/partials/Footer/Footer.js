681ee1452e4fd24863d1f8d5e0bdb7d160525032
crolim-vbi <crolim@vbirealestate.com>
2020-12-29 12:24:30 -0300

    refinamento

--- src/client/partials/Footer/Footer.js
+++ src/client/partials/Footer/Footer.js
@@ -15,7 +15,7 @@ const Footer = function() {
           </div>
           <div className="footer2 maxScreenWidth">
             <a style={{display: "inline-block", marginBottom: "10px"}}> Apollo27 é um sistema desenvolvido pela VBI Real Estate </a> <br></br>
-            <a style={{display: "inline-block", marginBottom: "10px"}}> COPYRIGHT © 2017 - 2020 - VBI REAL ESTATE - dev@vbirealestate.com</a> <br></br>
+            <a style={{display: "inline-block", marginBottom: "0px"}}> COPYRIGHT © 2017 - 2020 - VBI REAL ESTATE - dev@vbirealestate.com</a> <br></br>
           </div>       
         </Paper>
     );
