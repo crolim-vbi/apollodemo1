a0835d8ad8dfbbf90410075dc882c8bf50cd633e
crolim-vbi <crolim@vbirealestate.com>
2020-12-29 17:40:37 -0300

    refinamento

--- src/client/pages/Login/Login.js
+++ src/client/pages/Login/Login.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Navbar from '../../partials/Navbar/Navbar';
+import Footer from '../../partials/Footer/Footer';
+
 import Layout from '../../layout/Layout';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
@@ -6,31 +9,55 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import GoogleIcon from "../../shared/customIcons/GoogleIcon/GoogleIcon"
 
+import Paper from '@material-ui/core/Paper';
+
+
 import astronautWelcome from "./astronaut_welcome.png"
+
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
 
 
 const App = function() {
 
   
     return (
-      <div style={{textAlign: "center"}}>
-        <Layout titulo="Bem-Vindo ao Sistema Apollo Lunar" >
+      <div className="globalWrap">
+
           
-            <div style={{display: "inline-block", textAlign: "center"}}>
-
-              <img src={astronautWelcome} style={{width: "185px",  marginBottom: "25px"}}></img>
-
-              <div style={{color:"dimgray", marginBottom: "25px", fontSize: "1rem"}}>
-                  <a> Será necessária uma conta @vbirealestate.com, </a> <br></br>
-                  <a> caso possua, favor identifique-se no botão abaixo </a>
-              </div>
-              
-
-              <div style={{marginBottom: "25px", borderBottom: "solid 1px lightgray", width: "100%"}}> </div>
-              <Button href="/#/home" variant="contained">Login com o Google</Button>
-            </div>
+          <Navbar />
         
-        </Layout>
+          <div style={{display: "inline-block", textAlign: "center", margin: "auto"}}>
+
+              <div style={{borderBottom: "solid 1px lightgray2", marginBottom: "35px"}}>
+                <a style={{fontWeight: "600", fontSize: "1.25rem"}}>Bem-Vindo ao Sistema Apollo Lunar</a>
+              </div>
+
+          
+              <Paper elevation={2} style={{padding: "25px 25px 0px"}}>
+
+                {/* <img src={astronautWelcome} style={{width: "90px",  marginBottom: "25px"}}></img> */}
+
+                <AccountCircle style={{fontSize:"60px", color: "dimgray",  marginBottom: "25px"}}/>
+
+                <div style={{color:"dimgray", marginBottom: "25px", fontSize: "0.875rem"}}>
+                    <a> Será necessária uma conta @vbirealestate.com, </a> <br></br>
+                    <a> caso possua, favor identifique-se no botão abaixo </a>
+                </div>
+                
+
+                <div style={{marginBottom: "25px",  borderBottom: "solid 1px lightgray"}}> </div>
+
+                <Button href="/#/home" variant="contained" style={{marginBottom: "25px"}}>Login com o Google</Button>
+              
+              </Paper>
+          
+          </div>
+
+
+          <Footer />
+
+        
       </div>
     );
   }
