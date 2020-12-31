ca288e8f751b319e4c269b51b14572de7db6ba17
crolim-vbi <crolim@vbirealestate.com>
2020-12-31 02:16:55 -0300

    refinamento

--- src/client/pages/Preferencias.js
+++ src/client/pages/Preferencias.js
@@ -20,7 +20,7 @@ const App = function() {
             <a style={{color:"dimgray", fontSize: "0.875rem", marginBottom: "8px", display: "block"}} >
               Você será notificado via e-mail sobre os novos documentos conforme as regras de notificações cadastradas abaixo.</a>
             <a style={{color:"dimgray", fontSize: "0.875rem", marginBottom: "8px", display: "block"}} >
-              Mas você pode editar ou cadastrar novas regras conforme desejar. Favor, utilize as opções disponíveis também abaixo.</a>
+              Mas você pode editar ou cadastrar novas regras conforme desejar. Favor, utilize as opções disponíveis abaixo.</a>
           </div>
         
 
